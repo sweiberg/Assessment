736c5bbfa98defecdbc7f0aa75b4db528d82e2e0
sweiberg <sweiberg@ufl.edu>
2025-02-21 12:05:30 -0500

    Initial project push

---
+++ web/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ProductGrid from './components/productgrid/productgrid'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <ProductGrid />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ web/src/components/productgrid/productgrid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductGrid = () => {
+    return (
+        <div><h1>Test</h1></div>
+    );
+};
+
+export default ProductGrid
